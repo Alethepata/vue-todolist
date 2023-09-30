4dfa51f1d9dbb52a273408b78ca54481412de501
Alethepata <alessia.patane@gmail.com>
2023-09-30 16:46:32 +0200

    [Add] addTasks/ removeTask/done

--- js/script.js
+++ js/script.js
@@ -17,7 +17,26 @@ createApp({
                     task:'Ripassare Vue',
                     isDone:false
                 }
-            ]
+            ],
+            newTasks:
+                {
+                    task:'',
+                    isDone: false
+                }
+            
+        }
+    },
+    methods:{
+        addTasks(){
+            this.tasks.unshift(this.newTasks);
+            
+        },
+        removeTask(index){
+            this.tasks.splice(index,1)
+        },
+        done(task){
+            (task.isDone == false) ?  task.isDone= true : task.isDone = false
+
         }
     }
 
